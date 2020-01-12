dd97c68535b55f2f5ee6fbabff49adfff178ebbe
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-01-11 20:41:23 -0800

    removed jquery import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import 'bootstrap';
 import './scss/index.scss';
-import $ from 'jquery';
 
 import loadNavBar from './nav/nav';
 
 loadNavBar();
 
-$('#location').text(window.location.host);
+document.querySelector('#location').textContent = window.location.host;
