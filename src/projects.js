c882bbede9e3c2c09a1ceb6cfeaa7172d5074f8a
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-01-13 17:45:34 -0800

    added highlight to search function

--- src/projects.js
+++ src/projects.js
@@ -32,9 +32,20 @@ request.send();
 
 document.querySelector('#search').addEventListener('keyup', (eve) => {
   /** @type {string} */
-  const search = eve.target.value.toLowerCase();
+  const search = eve.target.value;
+  const searchRegex = new RegExp(search, 'ig');
   document.querySelectorAll('#projects .card-title').forEach((ele) => {
     const card = ele.parentElement.parentElement;
-    card.style.display = ele.textContent.toLowerCase().indexOf(search) > -1 ? '' : 'none';
+    const text = ele.textContent;
+    const innerEleWrapper = ele; // this is to shut up eslint
+    if (search === '') {
+      innerEleWrapper.innerHTML = innerEleWrapper.innerHTML.replace(/<mark>(.*?)<\/mark>/ig, '$1');
+      card.style.display = '';
+    } else if (text.match(searchRegex)) {
+      innerEleWrapper.innerHTML = text.replace(searchRegex, (match) => `<mark>${match}</mark>`);
+      card.style.display = '';
+    } else {
+      card.style.display = 'none';
+    }
   });
 });
