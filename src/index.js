d0b9a92349fd3a6655634524e44297bf796fa806
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-01-14 21:55:48 -0800

    added 3d sphere to front page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import 'core-js/stable';
 import loadNavBar from './nav/nav';
 
+import(/* webpackChunkName: "sphere" */ 'assembling-orange-sphere').then(({ default: aos }) => {
+  const canvas = document.querySelector('#animation');
+  aos.createAnimation(canvas);
+});
+
 /* eslint-disable no-unused-expressions */
 import(/* webpackChunkName: "bootstrap" */ 'bootstrap');
 import(/* webpackChunkName: "bootstrap-style" */'./scss/index.scss');
